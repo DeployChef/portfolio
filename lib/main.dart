9b67cae85a15e3f75f5394ec5538cab21fad6e6b
Vladislav <dc.titovvlad@gmail.com>
2022-07-20 13:41:20 +0300

    add assets, create home, build dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/pages/home/home.dart';
+import 'package:portfolio/utils/constants.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +16,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Portfolio',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: Theme.of(context).copyWith(
+        platform: TargetPlatform.android,
+        scaffoldBackgroundColor: kPrimaryColor,
+        canvasColor: kBackgroundColor,
+        textTheme: GoogleFonts.latoTextTheme(),
       ),
-      home: Container(),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget!),
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.resize(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: TABLET),
+          ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          ResponsiveBreakpoint.resize(2460, name: "4k"),
+        ],
+        background: Container(
+          color: kBackgroundColor,
+        ),
+      ),
+      home: Home(),
     );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+const Color kPrimaryColor = Color.fromARGB(21,181, 114, 1);
+const Color kBackgroundColor = Color.fromARGB(7, 17, 126, 1);
+const Color kDangerColor = Color.fromARGB(249, 77, 30, 1);
+const Color kCaptionColor = Color.fromARGB(166, 177, 187, 1);
